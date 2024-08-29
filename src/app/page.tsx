e39081848c9374d59df267bfc54be8e05b9e7e09
Kenny Ho <kenny850917@gmail.com>
2024-08-29 14:45:58 -0700

    fix minor css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,9 @@ const Home:React.FC=()=> {
   };
 
   return (
-    <main className="md:h-screen py-36 flex justify-center items-center bg-no-repeat bg-center bg-cover">
+    <main className="md:h-screen py-36 flex justify-center items-center">
       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-      <div className="container relative">
+      <div className="relative">
           <div className="max-w-[400px] w-full m-auto p-6 bg-white dark:bg-slate-900 shadow-md dark:shadow-gray-800 rounded-md">
             <h5 className="my-6 text-xl font-semibold justify-center flex">SIN Validator</h5>
               <p className="text-slate-400 mb-6">
@@ -36,12 +36,13 @@ const Home:React.FC=()=> {
               <form className="text-start" onSubmit={handleSubmit}>
 
                   <div className="mb-5">
-                    <label className="font-semibold">
+                    <label className="font-semibold" htmlFor="SIN">
                       Your SIN:
                     </label>
                     <input
+                      id="SIN"
                       type="text"
-                      className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
+                      className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600"
                       onChange={handleInputChange}
                       value={sin}
                       placeholder="Enter your SIN"
@@ -52,7 +53,7 @@ const Home:React.FC=()=> {
                   <div className="mb-4">
                     <button
                       type="submit"
-                      className={`py-2 px-5 inline-block tracking-wide border align-middle duration-500 text-base text-center rounded-md w-full 
+                      className={`py-2 px-5 inline-block duration-500 text-base text-center rounded-md w-full 
                       ${isDisabled ? 'bg-gray-400 text-gray-700 border-gray-400 cursor-not-allowed opacity-50' : 'bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white'}`}
                       disabled={isDisabled}
                     >
